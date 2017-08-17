0297d92370555eb1658dce565cc6eed471233a24
Brandon Zapata <zapatabrandon@gmail.com>
2017-08-17 16:40:06 -0400

    Script for updating database is almost complete

--- update_database.py
+++ update_database.py
@@ -20,18 +20,32 @@ def update_obj(obj, new_content):
 # project is true if the dir is the proj dir
 def check_files(project):
     files = os.listdir(os.curdir)
+    curr_dur = os.path.abspath(os.curdir)
     for f in files:
-        if os.listdir(f):
+        print("file name {}", f)
+        if os.path.isdir(f):
             raise Exception("There shouldn't be any folders where the markdown files are stored")
+        elif "draft" not in f and ".md" in f:
+            content = ""
+            dir = os.path.join(curr_dur, f)
+            with open(dir) as f:
+                content = f.read()
+            print(content)
+        # do nothing for everything else
+
+def strip_file_type(file_name):
+    if len(file_name) < 4:
+        raise Exception("File name too short should be more than 3 characters")
+    print("hello")
 
 def update():
     abs_pth = os.path.abspath(os.curdir)
     content_path = os.path.join(abs_pth, "content")
     print(content_path)
-    os.chdir("projects")
-    update(True)
-    os.chdir("../blogs")
-    update(False)
 
-update()
-
+    # chdir needs the full path name for whatever reason
+    # changing to prorojects
+    os.chdir(os.path.join(content_path, "projects"))
+    check_files(True)
+    os.chdir(os.path.join(content_path, "blogs"))
+    check_files(False)
